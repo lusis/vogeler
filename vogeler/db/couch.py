997c5df2d4b5815f872c9c9f2e3d661505908a09
John E. Vincent <lusis.org+github.com@gmail.com>
2010-09-15 08:16:21 -0400

    more odm abstraction. may not be working

--- vogeler/db/couch.py
+++ vogeler/db/couch.py
@@ -1,22 +1,92 @@
-import couchdbkit as couch
+import datetime, yaml
 
+import couchdbkit as couch
 from couchdbkit.loaders import FileSystemDocsLoader
 
 from vogeler.exceptions import VogelerException
 
 class SystemRecord(couch.Document):
     system_name = couch.StringProperty()
+    created_at = couch.DateTimeProperty()
     updated_at = couch.DateTimeProperty()
 
-def load(lp, database):
-    try:
-        print "Loading design docs from %s" % lp
-        loader = FileSystemDocsLoader(lp)
-        loader.sync(database, verbose=True)
-        print "Design docs loaded"
-        return 0
-    except:
-        raise VogelerException()
-        return 1
+class VogelerStore(object):
+
+    def __init__(self, **kwargs):
+        try:
+            host, port = kwargs['host'], kwargs['port']
+            db = kwargs['db']
+            connection_string = "http://%s:%s" % (host, port)
+            self.server = couch.Server(uri=connection_string)
+            self.create_db(db)
+            self.use_db(db)
+        except:
+            raise
+            #raise VogelerException()
+
+    def create_db(self, dbname):
+        try:
+            self.db = self.server.get_or_create_db(dbname)
+            SystemRecord.set_db(self.db)
+        except:
+            raise VogelerException()
+
+    def use_db(self, dbname):
+        try:
+            self.db = self.server.get_or_create_db(dbname)
+            SystemRecord.set_db(self.db)
+        except:
+            raise VogelerException()
+
+    def create(self, node_name):
+        node = SystemRecord.get_or_create(node_name)
+        try:
+            node.system_name = node_name
+            node.created_at = datetime.datetime.utcnow()
+            node.save()
+        except:
+            raise VogelerException()
+
+    def touch(self, node_name):
+        node = SystemRecord.get_or_create(node_name)
+        try:
+            node.updated_at = datetime.datetime.utcnow()
+            node.save()
+        except:
+            raise VogelerException()
+
+    def update(self, node_name, key, value, datatype):
+        node = SystemRecord.get_or_create(node_name)
+
+        if datatype == 'output':
+            v = [z.strip() for z in value.split("\n")]
+            node[key] = v
+        if datatype == 'pylist':
+            v = value
+            node[key] = v
+        if datatype == 'pydict':
+            v = value
+            node[key] = v
+        if datatype == 'yaml':
+            v = yaml.load(value)
+            node[key] = v
+        if datatype == 'string':
+            v = value
+            node[key] = v
+        if datatype == 'raw':
+            v = value
+            node[key] = v
+        node.updated_at = datetime.datetime.utcnow()
+        node.save()
+
+    def load_views(self, lp):
+        self.loadpath = lp
+        try:
+            print "Loading design docs from %s" % lp
+            loader = FileSystemDocsLoader(self.loadpath)
+            loader.sync(self.db, verbose=True)
+            print "Design docs loaded"
+        except:
+            raise VogelerException()
 
 # vim: set ts=4 et sw=4 sts=4 sta filetype=python :
--- vogeler/persistence.py
+++ vogeler/persistence.py
@@ -1,2 +1,44 @@
-import db.couch as couch
+import urlparse
+import vogeler.db.couch as couch
+
+from vogeler.exceptions import VogelerException
+
+"""vogeler.persisistence is used like so:
+
+import vogeler.persistence as engine
+# Create or find and then use the system_records db
+c = engine.create_engine('couch://127.0.0.1:5984/system_records')
+# create or find the nodename node
+c.create('nodename')
+# update the nodename node with the given attributes
+package_list = some_shell_command_output
+c.update('nodename', 'packages', package_list', 'output')
+mylist = ['foo','bar','baz']
+c.update('nodename', 'my_python_list', mylist, 'pylist')
+mydict = {'foo' : 1, 'bar' : 2, 'baz' : 'shoe'}
+c.update('nodename', 'my_python_dict', mydict, 'pydict')
+myyaml = some_yaml_data
+c.update('nodename', 'my_yaml_data', myyaml, 'yaml')
+c.update('nodename', 'my_string', 'some sting data', 'string')
+myxml = some_xml_data
+c.update('nodename', 'my_raw_data', myxml, 'raw')
+"""
+def create_engine(dsn, **credentials):
+    try:
+        scheme, params = _parse_url(dsn)
+        connect_string = "%s.VogelerStore(%s)" % (scheme, params)
+        engine = eval(connect_string)
+        return engine
+    except:
+        #raise VogelerException()
+        raise
+
+def _parse_url(url):
+    parsed = urlparse.urlparse(url)
+    s = parsed.scheme
+    h, p = parsed.netloc.split(':')
+    d = parsed.path.split("/")[1]
+    params = "host='%s', port='%s', db='%s'" % (h, p, d)
+    return (s, params)
+
 # vim: set ts=4 et sw=4 sts=4 sta filetype=python :
