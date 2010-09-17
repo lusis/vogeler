19d9e443a541550313026931c84a927bab5714d2
lusis <lusis.org+github.com@gmail.com>
2010-09-17 17:04:40 -0400

    working on updating docs

--- vogeler/client.py
+++ vogeler/client.py
@@ -4,6 +4,27 @@ from vogeler.exceptions import VogelerClientException
 from vogeler.messaging import amqp
 
 class VogelerClient(object):
+    """
+    Base class for operating as a Vogeler client instance
+
+    :param none callback_function: Callback function to use when messages are recieved
+
+    .. attribute:: callback_function
+
+        External callback used for processing messages
+
+    .. attribute:: ch
+
+        an instance of :class:`amqplib.client_0_8.channel.Channel`
+
+    .. attribute:: queue
+
+        an instance of :class:`amqplib.client_0_8.channel.Queue`
+
+    :raises: :class:`vogeler.exceptions.VogelerClientException`
+
+
+    """
     def __init__(self, callback_function=None, **kwargs):
         try:
             self.ch, self.queue = amqp.setup_client(kwargs['host'], kwargs['username'], kwargs['password'])
@@ -13,11 +34,27 @@ class VogelerClient(object):
                     Error connecting to %s as %s" % (kwargs['host'], kwargs['username']))
 
     def callback(self, msg):
-        message = json.loads(msg.body)
+        """
+        Wrapper method for handling callbacks on message reciept.
+        The message body is JSON decoded and passed up to :attr:`callback_function`
+        
+        :param msg: Instance of :class:`amqplib.client_0_8.basic_message.Message`
+
+        """
+        try:
+            message = json.loads(msg.body)
+        except:
+            print "Message not in JSON format"
+
         if(self.callback_function):
             self.callback_function(message)
 
     def monitor(self):
+        """
+        Method for watching a queue infinitely for messages
+
+        :raises: :class:`vogeler.exceptions.VogelerClientException`
+        """
         try:
             print "Vogeler(Client) is starting up"
             self.ch.basic_consume(self.queue, callback=self.callback, no_ack=True)
@@ -28,6 +65,18 @@ class VogelerClient(object):
             self.ch.wait()
 
     def message(self, message, durable=True):
+        """
+        Method for sending a message via the messaging system
+
+        :param message: The message to send.
+
+        :param bool durable: Sets the durable flag on the message causing it to persists
+
+        .. attribute:: msg 
+
+            instance of :class:`amqplib.client_0_8.basic_message.Message` wrapped in JSON
+
+        """
         print "Vogeler(Client) is sending a message"
         msg = amqp.amqp.Message(json.dumps(message))
         if durable == True:
